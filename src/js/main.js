d2d69b93659eca372459052e993fdcafa607c9a0
Yushenko Dima <damios.1812@gmail.com>
2025-03-25 17:27:25 +0100

    add new functions for forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,16 @@
 import './slider';
 import modal from './modules/modal';
 import { openModal } from './modules/modal';
-import tabs from './modules/tabs'
+import tabs from './modules/tabs';
+import forms from './modules/forms';
 
 document.addEventListener('DOMContentLoaded', () => {
+    "use strict";
     modal('.popup_engineer_btn', '.popup_engineer');
     modal('.contact_us_wrap', '.popup');
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
+    forms();
 })
 
 // contact_us_wrap popup
